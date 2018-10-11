0017eb3bf596b91a7dc2f0042222f8196d0431b5
Michael Kunze <mkunze@inoxio.de>
2018-10-11 20:28:43 +0200

    Set version number

--- src/main/java/org/mailster/smtp/SMTPServerConfig.java
+++ src/main/java/org/mailster/smtp/SMTPServerConfig.java
@@ -21,7 +21,7 @@ public class SMTPServerConfig {
     /**
      * Server version
      */
-    public static final String VERSION = "1.0.0-M3";
+    public static final String VERSION = "1.0.0";
 
     /**
      * 4 megs by default. The server will buffer incoming messages to disk
