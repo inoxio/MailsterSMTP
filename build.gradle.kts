cf76201717c841f20c942777ae46703fa5066aa5
Michael Kunze <mkunze@inoxio.de>
2018-10-11 20:26:01 +0200

    Add bintray plugin to release to bintray

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,16 @@
 import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
+import com.jfrog.bintray.gradle.BintrayExtension
+import com.jfrog.bintray.gradle.tasks.RecordingCopyTask
 import org.gradle.api.tasks.wrapper.Wrapper.DistributionType.ALL
 
 plugins {
     id("java")
     id("com.github.ben-manes.versions") version "0.20.0"
+    id("com.jfrog.bintray") version "1.8.4"
 }
+
+group = "de.inoxio"
+version = "1.0.0"
 
 java {
     sourceCompatibility = JavaVersion.VERSION_1_10
@@ -28,6 +34,36 @@ dependencies {
     testRuntime("org.apache.logging.log4j:log4j-slf4j-impl:2.11.1")
 }
 
+bintray {
+    user = properties["bintrayUser"] as String?
+            ?: System.getenv("BINTRAY_USER")
+    key = properties["bintrayApiKey"] as String?
+            ?: System.getenv("BINTRAY_API_KEY")
+
+    setConfigurations("archives")
+
+    publish = true
+
+    pkg(closureOf<BintrayExtension.PackageConfig> {
+        repo = "maven"
+        userOrg = "inoxio"
+        name = project.name
+
+        setLicenses("Apache-2.0")
+        issueTrackerUrl = "https://github.com/inoxio/MailsterSMTP/issues"
+        vcsUrl = "https://github.com/inoxio/MailsterSMTP.git"
+        githubRepo = "inoxio/MailsterSMTP"
+
+        version(closureOf<BintrayExtension.VersionConfig> {
+            name = project.version as String
+            vcsTag = project.version as String
+            gpg(closureOf<BintrayExtension.GpgConfig> {
+                sign = true
+            })
+        })
+    })
+}
+
 tasks {
     withType<JavaCompile> {
         options.apply {
@@ -42,6 +78,7 @@ tasks {
             componentSelection {
                 all {
                     if (listOf("alpha", "beta", "b01", "rc", "cr", "m")
+                                    .asSequence()
                                     .map { qualifier -> Regex("(?i).*[.-]$qualifier[.\\d-]*") }
                                     .any { it.matches(candidate.version) }) {
                         reject("Release candidate")
